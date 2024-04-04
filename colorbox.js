8d9015d02fa01ad0900f91a4f10243b4d639ee60
meenapothappagari <157195252+meenapothappagari@users.noreply.github.com>
2024-04-04 17:39:03 +0530

    Add files via upload

---
+++ colorbox.js
@@ -0,0 +1,54 @@
+let a=document.getElementById("colorbox")
+let a1=document.getElementById("red")
+a1.addEventListener("click",()=>{
+    a.style.backgroundColor="red"
+    a.style.color="white"
+})
+
+let b=document.getElementById("colorbox")
+let b1=document.getElementById("gold")
+b1.addEventListener("click",()=>{
+    b.style.backgroundColor="gold"
+    b.style.color="black"
+})
+
+let c=document.getElementById("colorbox")
+let c1=document.getElementById("grey")
+c1.addEventListener("click",()=>{
+    c.style.backgroundColor="grey"
+    c.style.color="white"
+})
+
+let d=document.getElementById("colorbox")
+let d1=document.getElementById("orange")
+d1.addEventListener("click",()=>{
+    d.style.backgroundColor="orange"
+    d.style.color="black"
+})
+
+let e=document.getElementById("colorbox")
+let e1=document.getElementById("black")
+e1.addEventListener("click",()=>{
+    e.style.backgroundColor="black"
+    e.style.color="white"
+})
+
+let f=document.getElementById("colorbox")
+let f1=document.getElementById("pink")
+f1.addEventListener("click",()=>{
+    f.style.backgroundColor="pink"
+    f.style.color="black"
+})
+
+let g=document.getElementById("colorbox")
+let g1=document.getElementById("Green")
+g1.addEventListener("click",()=>{
+    g.style.backgroundColor="Green"
+    g.style.color="white"
+})
+
+let h=document.getElementById("colorbox")
+let h1=document.getElementById("purple")
+h1.addEventListener("click",()=>{
+    h.style.backgroundColor="purple"
+})
